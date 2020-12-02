d4ad7fb22dcc37b24670cc00832a491fac8fc5a8
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-02 11:59:27 +0100

    bledne wyrazenie arytmetyczne

--- skrypt4.sh
+++ skrypt4.sh
@@ -6,6 +6,9 @@ read -p 'Podaj rok urodzenia: ' ur
 # taki zapis generalnie zadziala, ale jest gorszy i problematyczny w pewnych sytuacjach
 # wiek=$(( $rok - $ur ))
 
-wiek=$(( rok - ur ))
+# to juz nie zadziala, jesli x nie jest liczba:
+# wiek=$(( $rok - $ur + $x ))
+
+#wiek=$(( rok - ur ))
 
 echo "Twoj wiek to: $wiek"
