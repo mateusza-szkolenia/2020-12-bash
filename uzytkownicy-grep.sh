5f14a28b9e08e85f3ec64c704e56de6a0f883d6f
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-02 14:00:40 +0100

    opis

--- uzytkownicy-grep.sh
+++ uzytkownicy-grep.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 grep '^[a-z][a-z0-9]\{,7\}$' nazwy_uzytkownikow.txt
+#
+#     ^----------------------- początek linii
+#      ^^^^^------------------ dowolny znak z zakresu a-z
+#           ^^^^^^^^---------- dowolny znak z zakresu a-z lub 0-9
+#                   ^^--^^---- powtórzony
+#                     ^^------ od zera do 7 razy
+#                         ^--- koniec linii
