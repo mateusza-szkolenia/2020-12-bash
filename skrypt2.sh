d7f3227d96152e54a4d6e2ae019c47491c0fc918
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-02 11:21:59 +0100

    dwie zmienne

--- skrypt2.sh
+++ skrypt2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 read -p "Podaj imie: " imie
-echo "Dzien dobry, $imie"
+read -p "Podaj nazwisko: " nazwisko
+
+echo "Dzien dobry, $imie $nazwisko"
+echo "Imie: $imie"
+echo "Nazwisko: $nazwisko"
 
