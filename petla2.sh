a20cc779d6f45e95fd5a3971f8dda2fcc1e09094
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-02 15:01:36 +0100

    iteracja używająca seq

--- petla2.sh
+++ petla2.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-for i in 1 2 3 4 5 6 7 8 9 10
+#for i in 1 2 3 4 5 6 7 8 9 10
+#do
+#	echo "Liczę: $i"
+#	echo "..."
+#done
+
+for i in $( seq 1 10 )
 do
 	echo "Liczę: $i"
-	echo "..."
+	echo "....."
 done
 
