3ac24eb4f572c2a07b5e4b9f9d2a63d5bfd4653b
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-02 16:44:54 +0100

    funkcje w bashu

---
+++ funkcje1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nazwa="SuperProgram"
+
+powitanie(){
+	echo "Witaj w programie $nazwa"
+	echo " "
+	echo "(C) Copyleft 2020 Mateusz A."
+}
+
+powitanie
---
+++ funkcje2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+abc(){
+	echo "Liczba parametrów: $#"
+	echo "Par 1: $1"
+	echo "Par 2: $2"
+	echo "Par 3: $3"
+	echo "Par 4: $4"
+}
+
+echo "Liczba parametrów: $#"
+echo "Par 1: $1"
+echo "Par 2: $2"
+
+abc "$@"
+#mkdir "$@"
